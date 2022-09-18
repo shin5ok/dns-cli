3b44abe0fcd1f9f17d63818332023d5d61714551
shin5ok <shin5ok@55mp.com>
2022-09-18 22:25:00 +0900

    specify domain name will be configured

--- main.go
+++ main.go
@@ -3,25 +3,36 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	dns "github.com/shin5ok/dnscli/internal/dns"
 )
 
+var defaultConfig = map[string]string{
+	"domain": os.Getenv("DNS_DOMAIN"),
+	"ttl":    os.Getenv("DNS_DEFAULT_TTL"),
+}
+
 func main() {
 	data := flag.String("data", "", "")
 	key := flag.String("key", "", "")
+	domain := flag.String("domain", defaultConfig["domain"], `Do "export DNS_DOMAIN=<your domain>" to set default managed domain`)
+	ttl := flag.Int64("ttl", 60, "")
 	flag.Parse()
 	rr := dns.Record{
 		RType: "A",
 		RData: *data,
 		RKey:  *key,
-		TTL:   60,
+		TTL:   int(*ttl),
 	}
 
-	dnsRr := dns.Rr{}
+	dnsRr := dns.Rr{
+		Domain: *domain,
+	}
 	err := dnsRr.Set(&rr)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 	fmt.Printf("Registered: %+v\n", rr)
 }
