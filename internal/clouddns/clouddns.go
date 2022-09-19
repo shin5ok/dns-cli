c3667722265f66fab66de99ebf8ea08c74969fb5
shin5ok <shin5ok@55mp.com>
2022-09-19 13:35:09 +0900

    work it with Cloud DNS API

--- internal/clouddns/clouddns.go
+++ internal/clouddns/clouddns.go
@@ -1,8 +1,16 @@
 package clouddns
 
+import (
+	"context"
+	"log"
+
+	"golang.org/x/oauth2/google"
+	dns "google.golang.org/api/dns/v1"
+)
+
 type Record struct {
 	RType  string
-	RData  string
+	RData  []string
 	RKey   string
 	TTL    int
 	Status string
@@ -13,16 +21,36 @@ type Recorder interface {
 	Set(*Record) error
 }
 
-type Rr struct {
-	Domain string
+type ZoneInfo struct {
+	Domain      string
+	ProjectId   string
+	ManagedZone string
 }
 
-func (r *Rr) Get(key string) (*Record, error) {
+func (i *ZoneInfo) Get(key string) (*Record, error) {
 	return &Record{}, nil
 }
 
-func (r *Rr) Set(*Record) error {
-	// ctx := context.Background()
-	// dnsService, err := dns.NewService(ctx, option.WithScopes(dns.NdevClouddnsReadwriteScope))
+func (i *ZoneInfo) Set(r *Record) error {
+
+	ctx := context.Background()
+	c, err := google.DefaultClient(ctx, dns.CloudPlatformScope)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dnsService, err := dns.New(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	recordSet := dns.ResourceRecordSet{
+		Name:    r.RKey,
+		Rrdatas: r.RData,
+	}
+	// log.Println(i.ProjectId, i.ManagedZone, r.RKey, r.RType, &recordSet)
+	_, err = dnsService.ResourceRecordSets.Patch(i.ProjectId, i.ManagedZone, r.RKey, r.RType, &recordSet).Context(ctx).Do()
+	if err != nil {
+		return err
+	}
 	return nil
 }
