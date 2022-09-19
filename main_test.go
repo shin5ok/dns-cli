5a173bea025c07252367509150b7ae025f69881c
shin5ok <shin5ok@55mp.com>
2022-09-20 00:28:28 +0900

    make sure if it runs

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/shin5ok/dnscli/internal/clouddns"
@@ -9,7 +10,7 @@ import (
 func Test_main(t *testing.T) {
 	setRecord := clouddns.Record{
 		RType:  "A",
-		RData:  "192.168.0.1",
+		RData:  []string{"192.168.0.1"},
 		RKey:   "foo",
 		TTL:    60,
 		Status: "OK",
@@ -23,7 +24,8 @@ func Test_main(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	if getRecord.RData != "192.168.0.1" {
+	if !reflect.DeepEqual(getRecord.RData, []string{"192.168.0.1"}) {
+		t.Error("RData:", getRecord.RData)
 		t.Error("getRecord.RData is not equal expected")
 	}
 	t.Log(getRecord)
@@ -35,7 +37,7 @@ type MockRr struct {
 func (r *MockRr) Get(key string) (*clouddns.Record, error) {
 	return &clouddns.Record{
 		RType:  "A",
-		RData:  "192.168.0.1",
+		RData:  []string{"192.168.0.1"},
 		RKey:   "foo",
 		TTL:    60,
 		Status: "OK",
